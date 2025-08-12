117ae1ce617ff92d9becae7a18ff83a065e3bb3d
wokron <yi1tang.yang@gmail.com>
2025-08-12 08:32:02 +0800

    send profiling event to queue

--- core/ebpf/plugin/cpu_profiling/CpuProfilingManager.cpp
+++ core/ebpf/plugin/cpu_profiling/CpuProfilingManager.cpp
@@ -13,7 +13,10 @@
 // limitations under the License.
 
 #include "ebpf/plugin/cpu_profiling/CpuProfilingManager.h"
-#include "CpuProfilingManager.h"
+#include "collection_pipeline/CollectionPipelineContext.h"
+#include "collection_pipeline/queue/ProcessQueueItem.h"
+#include "collection_pipeline/queue/ProcessQueueManager.h"
+#include "common/queue/blockingconcurrentqueue.h"
 #include "ebpf/plugin/cpu_profiling/ProcessScanner.h"
 
 namespace logtail {
@@ -91,6 +94,12 @@ int CpuProfilingManager::AddOrUpdateConfig(
         .mRegexs = opts->mCmdlines,
         .mCallback =
             [this](std::vector<uint32_t> pids) {
+                std::string pidsStr;
+                for (auto pid : pids) {
+                    pidsStr += std::to_string(pid) + ",";
+                }
+                LOG_DEBUG(sLogger, ("CpuProfilingManager update config",
+                                    "")("pids", pidsStr));
                 mEBPFAdapter->UpdatePlugin(
                     PluginType::CPU_PROFILING,
                     buildCpuProfilingConfig(std::move(pids), nullptr, nullptr));
@@ -117,14 +126,81 @@ int CpuProfilingManager::Suspend() {
     return 0;
 }
 
+using StackCnt = std::pair<std::string, uint32_t>;
+
+static void parseStackCnt(char const *symbol, std::vector<StackCnt> &result) {
+    // Format: "<comm>:<pid>;<stacks> <cnt>\n"
+    // Example: "bash:1234;func1;func2;func3 10\n"
+
+    std::istringstream ssymbol;
+    ssymbol.str(symbol);
+    std::string line;
+    while (std::getline(ssymbol, line)) {
+        auto pos1 = line.find(';');
+        if (pos1 == std::string::npos) {
+            LOG_ERROR(sLogger, ("Invalid symbol format", line));
+            continue;
+        }
+        auto pos2 = line.find(' ');
+        if (pos2 == std::string::npos || pos2 < pos1) {
+            LOG_ERROR(sLogger, ("Invalid symbol format", line));
+            continue;
+        }
+
+        auto stack = line.substr(pos1 + 1, pos2 - pos1 - 1);
+        auto cntStr = line.substr(pos2 + 1);
+        uint32_t cnt = std::stoul(cntStr);
+
+        result.push_back(std::make_pair(stack, cnt));
+    }
+}
+
 void CpuProfilingManager::HandleCpuProfilingEvent(uint32_t pid,
                                                   const char *comm,
                                                   const char *stack,
                                                   uint32_t cnt) {
-    LOG_INFO(sLogger,
-             ("CpuProfilingManager HandleCpuProfilingEvent",
-              "")("pid", std::to_string(pid))("comm", std::string(comm))(
-                 "stack", std::string(stack))("cnt", std::to_string(cnt)));
+    LOG_DEBUG(sLogger, ("CpuProfilingEvent", "")("pid", pid)("comm", comm)(
+                           "stack", stack)("cnt", cnt));
+
+    std::vector<StackCnt> stacks;
+    parseStackCnt(stack, stacks);
+
+    auto sourceBuffer = std::make_shared<SourceBuffer>();
+    PipelineEventGroup eventGroup(sourceBuffer);
+    for (auto &[stack, cnt] : stacks) {
+        auto* event = eventGroup.AddLogEvent();
+        event->SetContent("pid", std::to_string(pid));
+        event->SetContent("comm", std::string(comm));
+        event->SetContent("stack", std::string(stack));
+        event->SetContent("cnt", std::to_string(cnt));
+    }
+
+    {
+        if (!mPipelineCtx) {
+            return;
+        }
+
+        std::unique_ptr<ProcessQueueItem> item =
+            std::make_unique<ProcessQueueItem>(std::move(eventGroup),
+                                               mPluginIndex);
+        int maxRetry = 5;
+        for (int retry = 0; retry < maxRetry; ++retry) {
+            if (QueueStatus::OK ==
+                ProcessQueueManager::GetInstance()->PushQueue(
+                    mQueueKey, std::move(item))) {
+                break;
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            if (retry == maxRetry - 1) {
+                LOG_WARNING(
+                    sLogger,
+                    ("configName", info.mPipelineCtx->GetConfigName())(
+                        "pluginIdx", info.mPluginIndex)(
+                        "[CpuProfilingEvent] push queue failed!", ""));
+                // TODO: Alarm discard data
+            }
+        }
+    }
 };
 
 } // namespace ebpf
