e70e1298ebc82af59a77acf108aa63447bf29830
wokron <yi1tang.yang@gmail.com>
2025-08-19 14:35:38 +0800

    remove unnecessary set to vector conversion

--- core/ebpf/include/export.h
+++ core/ebpf/include/export.h
@@ -7,6 +7,7 @@
 #include <string>
 #include <variant>
 #include <vector>
+#include <unordered_set>
 
 extern "C" {
 #include <coolbpf/net.h>
@@ -158,7 +159,7 @@ struct FileSecurityConfig {
 using CpuProfilingHandler = void (*)(uint32_t pid, const char *comm, const char *stack, uint32_t cnt, void *ctx);
 
 struct CpuProfilingConfig {
-    std::vector<uint32_t> mPids;
+    std::unordered_set<uint32_t> mPids;
     CpuProfilingHandler mHandler;
     void *mCtx;
 };
